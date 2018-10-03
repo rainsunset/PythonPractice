bb3634dfa8a34b1929018565b48febb92a690479
rainsunset <15620608789@163.com>
2018-10-03 22:08:14 +0800

    first commit

---
+++ hello.py
@@ -0,0 +1,23 @@
+# 控制台输出函数
+print('hello world')
+# 内容也可以为用逗号隔开的的多个内容 显示结果为空格隔开
+print('helloWorld','cheer up','奈斯')
+# 打印函数也可以用于计算
+print(300 + 2)
+# So What will happen 
+print("300 + 2 =",300+2)
+# ================================
+
+# 输入函数
+print('waitting input ...')
+input()
+print('input end')
+# ================================
+
+# 变量
+print('计算乘法 \n 输入因子a')
+a = input()
+print('输入因子b')
+b = input()
+print(a,'*',b,'=',int(a) * int(b))
+# ================================
